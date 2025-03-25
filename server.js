ecd5270c244bf82fade583082a066471dcd3da84
Arleene145 <158856381+Arleene145@users.noreply.github.com>
2025-03-24 21:09:24 -0600

    Update server.js
    Se agrega lo respectivo al proyecto de la semana 4

--- server.js
+++ server.js
@@ -22,3 +22,20 @@ app.listen(PORT, () => {
 });
 const habitosRoutes = require("./routes/habitos");
 app.use("/habitos", habitosRoutes);
+
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("MongoDB Conectado"))
+  .catch(err => console.log(err));
+
+app.listen(5000, () => console.log("Servidor corriendo en puerto 5000"));
